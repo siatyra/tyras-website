d50657f1e90944f50fe12206b5e256304a46bf49
siatyra <sia.anglin97@gmail.com>
2024-10-19 18:03:45 +0100

    use different star icon

--- src/app/animations/spinningDiamond.tsx
+++ src/app/animations/spinningDiamond.tsx
@@ -1,3 +1,5 @@
+import Icon from '@mdi/react';
+import { mdiStarFourPoints } from '@mdi/js';
 import { motion } from "framer-motion";
 import { SpinningDiamondProps } from "../interfaces";
 
@@ -13,6 +15,7 @@ export const SpinningDiamond: React.FC<SpinningDiamondProps> = ({
       width={size}
       x="0px"
       y="0px"
+      preserveAspectRatio="xMidYMid meet"
       animate={{
         rotate: rotate,
         opacity: 1,
@@ -24,10 +27,7 @@ export const SpinningDiamond: React.FC<SpinningDiamondProps> = ({
         repeatDelay: 0,
       }}
     >
-      <motion.path
-        fill="#FFFFFF"
-        d="M86.67,50c0,.25-.18,.46-.43,.49-28.21,4.19-31.55,7.54-35.74,35.74-.04,.24-.25,.43-.49,.43s-.46-.18-.49-.43c-4.19-28.21-7.54-31.55-35.74-35.74-.24-.04-.43-.25-.43-.49s.18-.46,.43-.49c28.21-4.19,31.55-7.54,35.74-35.74,.04-.24,.25-.43,.49-.43s.46,.18,.49,.43c4.19,28.21,7.54,31.55,35.74,35.74,.24,.04,.43,.25,.43,.49Z"
-      />
+    <Icon path={mdiStarFourPoints} size="100%" color="#FFFFFF" />
     </motion.svg>
   );
 };
