2895c43fe4af2d72bf200bae736c3f7806980598
siatyra <sia.anglin97@gmail.com>
2024-10-19 17:30:55 +0100

    create a wrapper component

---
+++ src/app/assets/Wrapper.tsx
@@ -0,0 +1,70 @@
+import { cva, type VariantProps } from "class-variance-authority";
+
+type WrapperProps = {
+  children: React.ReactNode;
+} & VariantProps<typeof wrapperCva>;
+
+export const wrapperCva = cva(["flex"], {
+  variants: {
+    align: {
+      center: "items-center",
+      start: "items-start",
+      end: "items-end",
+    },
+    justify: {
+      center: "justify-center",
+      start: "justify-start",
+      end: "justify-end",
+      apart: "justify-between",
+      together: "justify-around",
+    },
+    direction: {
+      row: "flex-row",
+      column: "flex-col",
+    },
+    gap: {
+      none: "gap-0",
+      tiny: "gap-1",
+      small: "gap-2",
+      medium: "gap-4",
+      large: "gap-8",
+    },
+    spacer: {
+      medium: "mb-4",
+      large: "mb-8",
+    },
+    fill: {
+      width: "w-full",
+      height: "h-full",
+    },
+  },
+  defaultVariants: {
+    direction: "column",
+    gap: "large",
+  },
+});
+
+export const Wrapper = ({
+  children,
+  align,
+  justify,
+  direction,
+  gap,
+  spacer,
+  fill,
+}: WrapperProps) => {
+  return (
+    <div
+      className={wrapperCva({
+        align,
+        justify,
+        direction,
+        gap,
+        spacer,
+        fill,
+      })}
+    >
+      {children}
+    </div>
+  );
+};
